92d25baf35789e696dbbb302e5877a9f0fe79f21
maxing <xm.crazyboy@gmail.com>
2014-01-19 18:30:28 +0800

    remove AudioManager::Init

--- Classes/AppDelegate.cpp
+++ Classes/AppDelegate.cpp
@@ -48,7 +48,6 @@ bool AppDelegate::applicationDidFinishLaunching() {
     pDirector->setAnimationInterval(1.0 / 60);
 
     Record::instance()->load();
-    AudioManager::init();
     // create a scene. it's an autorelease object
     //CCScene *pScene = CCScene::create();
     //GameLayer *pLayer = GameLayer::create();
